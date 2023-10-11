bf661a079c3d8c42d919982a5fa5d229116a90e7
dodor <choidy9298@naver.com>
2023-10-11 17:32:37 +0900

    Initial Commit

---
+++ UIKitBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitBasic
+//
+//  Created by dodor on 2023/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
