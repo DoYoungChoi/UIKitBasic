a58a989f120eb8a372a4039652bd3cc90413f19a
dodor <choidy9298@naver.com>
2023-10-12 13:56:19 +0900

    Part2. UI Kit 기초 5-2. Delegate 친해지기

--- UIKitBasic/MyViewController.swift
+++ UIKitBasic/MyViewController.swift
@@ -7,19 +7,36 @@
 
 import UIKit
 
+protocol AdminDelegate {
+    func doTask()
+}
+
 class MyViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var helloLabel: UILabel!
+    var admin: Admin?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        admin = Admin(delegate: self)
     }
     
     @IBAction func didTabButton(_ sender: Any) {
         if let name = nameTextField.text {
             helloLabel.text = "\(name)님 안녕하세요!"
         }
+        admin?.delegate.doTask()
     }
+}
+
+extension MyViewController: AdminDelegate {
+    func doTask() {
+        print("일 하는 중...")
+    }
+}
+
+struct Admin {
+    var delegate: AdminDelegate
 }
